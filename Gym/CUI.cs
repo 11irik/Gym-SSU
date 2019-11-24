fcac0e33eb0f5c39af634d86322872d7bb188005
Kirill <lukashinkv@gmail.com>
2019-11-24 21:48:55 +0400

    missed files from previous commit

---
+++ Gym/CUI.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+
+namespace Gym
+{
+  internal class CUI
+  {
+        public static void Main(string[] args)
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Gym));
+            Gym gym = new Gym(); 
+            /*using (FileStream fs = new FileStream("gym.json", FileMode.OpenOrCreate))
+            {
+                gym = (Gym)jsonFormatter.ReadObject(fs);
+            }*/
+
+            int caseSwitch;
+            bool isEnd = false;
+            List<String> strings = new List<string>();
+
+            ShowHelp();
+         
+            while (!isEnd)
+            {
+                caseSwitch = int.Parse(Console.ReadLine());
+                strings.Clear();
+                switch (caseSwitch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter trainer's full name:");
+                        Console.Write("Enter lastname: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter name: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter patronymic: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter trainer's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.AddTrainer(strings[0], strings[1], strings[2], strings[3]);
+                        break;
+                    case 2:
+                        Console.Write("Enter room type ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.AddRoom(strings[0], int.Parse(strings[1]));
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter client's full name:");
+                        Console.Write("Enter lastname: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter name: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter patronymic: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter client's phone number:");
+                        strings.Add(Console.ReadLine());
+                        gym.AddClient(strings[0], strings[1], strings[2], strings[3]);
+                        break;
+                    case 4:
+                        Console.Write("Enter trainer's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.RemoveTrainer(strings[0]);
+                        break;
+                    case 5:
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.RemoveRoom(int.Parse(strings[0]));
+                        break;
+                    case 6:
+                        Console.Write("Enter client's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.RemoveClient(strings[0]);
+                        break;
+                    case 7:
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter day ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter time ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter trainer's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.SetTraining(int.Parse(strings[0]), int.Parse(strings[1]), int.Parse(strings[2]), strings[3]);
+                        break;
+                    case 8:
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter day ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter time ");
+                        strings.Add(Console.ReadLine());
+                        gym.RemoveTraining(int.Parse(strings[0]), int.Parse(strings[1]), int.Parse(strings[2]));
+                        break;
+                    case 9:
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter day ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter time ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter client's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.WriteClient(int.Parse(strings[0]), int.Parse(strings[1]), int.Parse(strings[2]), strings[3]);
+                        break;
+                    case 10:
+                        Console.Write("Enter room number: ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter day ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter time ");
+                        strings.Add(Console.ReadLine());
+                        Console.Write("Enter client's phone number: ");
+                        strings.Add(Console.ReadLine());
+                        gym.WriteOutClient(int.Parse(strings[0]), int.Parse(strings[1]), int.Parse(strings[2]), strings[3]);
+                        break;
+                    case 11:
+                        Console.WriteLine(gym.GetTrainers());
+                        break;
+                    case 12:
+                        Console.WriteLine(gym.GetRooms());
+                        break;
+                    case 13:
+                        Console.WriteLine(gym.GetClients());
+                        break;
+                    case 14:
+                        Console.WriteLine(gym.GetSchedule());
+                        break;
+                    case 111:
+                        ShowHelp();
+                        break;
+                    case 100:
+                        isEnd = true;
+                        return;
+                }
+                Console.Write("Enter number of a command:");
+            }
+        }
+
+        static void ShowHelp()
+        {
+            String[] methodNames = {"Add trainer to base - 1", "Add room to base - 2", "Add client to base - 3", "Remove trainer - 4", "Remove room - 5",
+                "Remove client - 6", "Set training - 7", "Remove training - 8", "Write client - 9", "Writeout client - 10", "Show trainer base - 11",
+                "Show room base - 12", "Show client base - 13", "Show schedule - 14", "Show help - 111", "Stop - 100"};
+
+            String s = "";
+            for (int i = 0; i < methodNames.Length; ++i)
+            {
+                s += String.Format("{0, 25}\t", methodNames[i]);
+                if (i % 2 != 0)
+                {
+                    s += "\n";
+                }
+            }
+            Console.WriteLine(s);
+        }
+
+    }
+}
---
+++ Gym/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Gym
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        protected string _lastname { get; set; }
+        [DataMember]
+        protected string _name { get; set; }
+        [DataMember]
+        protected string _patronymic { get; set; }
+        [DataMember]
+        protected string _phonenumber { get; set; }
+        [DataMember]
+        protected int _id { get; }
+
+        private static int _idCounter = 0;
+
+        public Person(string surname, string name, string patronymic, string phoneNumber)
+        {
+            _lastname = surname;
+            _name = name;
+            _patronymic = patronymic;
+            _phonenumber = phoneNumber;
+            _id = ++_idCounter;
+        }
+
+        public string Phonenumber
+        {
+            get => _phonenumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{_lastname};{_name};{_patronymic};{_phonenumber};";
+        }
+    }
+}
